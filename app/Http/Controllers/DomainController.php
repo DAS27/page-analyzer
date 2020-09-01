1424e5d467dfcfb5c2415aa7c4f20ceac0c00fec
Damir Sarkulin <sarkulindamir@gmail.com>
2020-09-02 01:25:27 +0500

    wip v.3

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\DomainRequest;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class DomainController extends Controller
 {
     public function check(DomainRequest $request)
     {
-        $domain = DB::insert(INTO 'pageanalyzer' );
+//        DB::table('pageanalyzer')
+//            ->insert(['id' => 1, 'name' => $request->input('domain.name'), 'created_at' => Carbon::now(), 'updated_at' => Carbon::tomorrow()]
+//            );
+
+        $domains = DB::table('pageanalyzer')->get();
+        dd($domains);
     }
 }
