5edc2300ce5aedf009f2a46a1518d537b5410d72
Damir Sarkulin <sarkulindamir@gmail.com>
2020-09-02 21:09:38 +0500

    added domains list page

--- resources/views/domains.blade.php
+++ resources/views/domains.blade.php
@@ -6,24 +6,26 @@
         <div class="container-lg">
             <h1 class="mt-5 mb-3">Domains</h1>
             <div class="table-responsive">
-                @foreach($domains as $domain)
                 <table class="table table-bordered table-hover text-nowrap">
-                    <tbody>
+                    <thead>
                         <tr>
                             <th>ID</th>
                             <th>Name</th>
                             <th>Last check</th>
                             <th>Status Code</th>
                         </tr>
+                    </thead>
+                    @foreach($domains as $domain)
+                    <tbody>
                         <tr>
                             <td>{{ $domain->id }}</td>
                             <td>{{ $domain->name }}</td>
                             <td>{{ $domain->updated_at }}</td>
-                            <td>{{ $domain->updated_at }}</td>
+                            <td>{{ 200 }}</td>
                         </tr>
                     </tbody>
+                    @endforeach
                 </table>
-                @endforeach
             </div>
         </div>
     </main>
