58704d1d85603384570d77259c6f6bece1d05021
Damir <sarkulindamir@gmail.com>
2020-09-14 17:41:36 +0500

    updated link to home page

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="https://peaceful-stream-47620.herokuapp.com/">Analyzer</a>
+        <a class="navbar-brand" href="{{ route('home') }}">Analyzer</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
