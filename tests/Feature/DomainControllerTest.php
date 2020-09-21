51efdaea7cc5695514a4721246f5b792d6005cde
Damir <sarkulindamir@gmail.com>
2020-09-21 12:17:59 +0500

    fixed tests

--- tests/Feature/DomainControllerTest.php
+++ tests/Feature/DomainControllerTest.php
@@ -4,20 +4,12 @@ namespace Tests\Feature;
 
 use Faker\Factory;
 use Faker\Provider\Base;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class DomainControllerTest extends TestCase
 {
-    use DatabaseMigrations;
-    use WithoutMiddleware;
-
-    private const DOMAIN = ['name' => 'https://ru.hexlet.io/professions/php/projects/9'];
-    private const EXPECT_DOMAIN = ['name' => 'https://ru.hexlet.io'];
-
     protected function setUp(): void
     {
         parent::setUp();
@@ -33,10 +25,12 @@ class DomainControllerTest extends TestCase
 
     public function testStore()
     {
-        $response = $this->post(route('domains.store', self::DOMAIN));
+        $domain = ['name' => 'https://ru.hexlet.io/professions/php/projects/9'];
+        $expect  = ['name' => 'https://ru.hexlet.io'];
+        $response = $this->post(route('domains.store', $domain));
         $response->assertSessionHasNoErrors();
         $response->assertRedirect();
-        $this->assertDatabaseHas('domains', self::EXPECT_DOMAIN);
+        $this->assertDatabaseHas('domains', $expect);
     }
 
     public function testShow()
@@ -51,20 +45,15 @@ class DomainControllerTest extends TestCase
         $domainName = DB::table('domains')->inRandomOrder()->first('name')->name;
         $data = file_get_contents(__DIR__ . '/../fixtures/test.html');
         Http::fake([
-            $domainName => Http::response($data, 404)
+            $domainName => Http::response($data, 200)
         ]);
-        $id = DB::table('domains')->where('name', $domainName)->pluck('id')->all()[0];
+        $id = DB::table('domains')->where('name', $domainName)->pluck('id')->first();
         $response = $this->post(route('domain.check', $id));
-        $statusCode = DB::table('domain_checks')->where('domain_id', $id)->pluck('status_code')->all()[0];
-        $h1 = DB::table('domain_checks')->where('domain_id', $id)->pluck('h1')->all()[0];
-        $keywords = DB::table('domain_checks')->where('domain_id', $id)->pluck('keywords')->all()[0];
-        $description = DB::table('domain_checks')->where('domain_id', $id)->pluck('description')->all()[0];
+        $h1 = ['h1' => 'This is H1'];
+        $description = ['description' => 'This is Description'];
+        $expect = array_merge($h1, $description);
         $response->assertSessionHasNoErrors();
         $response->assertRedirect();
-        $this->assertDatabaseHas('domain_checks', ['domain_id' => $id]);
-        $this->assertDatabaseHas('domain_checks', ['status_code' => $statusCode]);
-        $this->assertDatabaseHas('domain_checks', ['h1' => $h1]);
-        $this->assertDatabaseHas('domain_checks', ['keywords' => $keywords]);
-        $this->assertDatabaseHas('domain_checks', ['description' => $description]);
+        $this->assertDatabaseHas('domain_checks', $expect);
     }
 }
