60e2d274faa9b17999d7fd9c33f7c708a7f498dc
Damir <sarkulindamir@gmail.com>
2020-09-24 17:06:52 +0500

    fixed query

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -2,21 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use DiDom\Document;
-use GuzzleHttp\Exception\RequestException;
-use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 
 class DomainController extends Controller
 {
     public function index()
     {
-        $domains = DB::table('domains')->get();
+        $domains = DB::table('domains')->paginate(10);
+        $domainIds = $domains->pluck('id');
         $lastChecks = DB::table('domain_checks')
+            ->whereIn('domain_id', $domainIds)
             ->distinct('domain_id')
             ->orderBy('domain_id')
             ->orderBy('updated_at', 'asc')
@@ -43,7 +41,7 @@ class DomainController extends Controller
         $normalizedUrl = strtolower("{$scheme}://{$host}");
         $domain = DB::table('domains')->where('name', $normalizedUrl)->first();
         if ($domain) {
-            flash('Url already exists')->warning();
+            flash('Url already exists')->success();
             return redirect()->route('domain.show', ['id' => $domain->id]);
         }
         $id = DB::table('domains')->insertGetId(
@@ -63,38 +61,8 @@ class DomainController extends Controller
         abort_unless($domain, 404);
         $domainChecks = DB::table('domain_checks')
             ->where('domain_id', $id)
-            ->paginate(10);
+            ->orderBy('id', 'desc')
+            ->paginate(5);
         return view('show', compact('domain', 'domainChecks'));
-    }
-
-    public function check($id)
-    {
-        $domain = DB::table('domains')->find($id);
-        abort_unless($domain, 404);
-        try {
-            $response = Http::get($domain->name);
-            $document = new Document($response->body());
-            $h1 = optional($document->first('h1'))->text();
-            $keywords = optional($document->first('*[^name=keywords]'))->getAttribute('content');
-            $description = optional($document->first('*[^name=description]'))->getAttribute('content');
-            $statusCode = $response->status();
-            DB::table('domain_checks')->insert(
-                [
-                    'domain_id' => $id,
-                    'status_code' => $statusCode,
-                    'h1' => $h1,
-                    'keywords' => $keywords,
-                    'description' => $description,
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now()
-                ]
-            );
-            flash('Website has been checked!');
-        } catch (RequestException $e) {
-            flash('Website not found')->error();
-        } catch (ConnectionException $e) {
-            flash('Oops something went wrong')->error();
-        }
-        return redirect()->route('domain.show', ['id' => $id]);
     }
 }
